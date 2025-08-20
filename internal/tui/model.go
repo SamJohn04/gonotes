4354818fa5b0e7f1bb17a8bcebfbb6e02c19fcff
SamJohn04 <samuel.john.codes@gmail.com>
2025-08-20 07:55:00 +0530

    feat(in progress): add a single line of command

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -5,12 +5,14 @@ import tea "github.com/charmbracelet/bubbletea"
 type model struct {
 	cursor int
 	notes []string
+	command string
 }
 
 func InitialModel() model {
 	return model{
 		cursor: 0,
 		notes: []string{},
+		command: "",
 	}
 }
 
@@ -18,18 +20,39 @@ func (m model) Init() tea.Cmd {
 	return nil
 }
 
-func (m model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
-	switch message := message.(type) {
-		case tea.KeyMsg:
-			if message.String() == "ctrl+c" {
-				return m, tea.Quit
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		cmd := m.updateKeyMessage(msg)
+		if cmd != nil {
+			return m, cmd
 		}
 	}
 	return m, nil
 }
 
 func (m model) View() string {
-	s := "Press ctrl+c to quit\n\n"
+	s := "Press ctrl+c to quit\n"
+
+	if m.command != "" {
+		s += m.command
+	}
+
+	s += "\n"
 
 	return s
+}
+
+func (m *model) updateKeyMessage(msg tea.KeyMsg) (tea.Cmd) {
+	switch msg.String() {
+	// exit the program
+	case "ctrl+c":
+		return tea.Quit
+	case "enter":
+		m.command = ""
+		return nil
+	default:
+		m.command += msg.String()
+		return nil
+	}
 }
